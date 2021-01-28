9b635a4b289859871a7751574e6962cdf24169a2
Isadora Brito <isadoradpbrito@gmail.com>
2021-01-28 08:17:11 -0300

    lista02 - complementares

---
+++ Ex1933.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Ex1933 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int carta1,carta2;
+
+        System.out.println("Digite o valor das duas cartas que você já possui: ");
+        carta1 = in.nextInt();
+        carta2 = in.nextInt();
+
+        if (carta1 > carta2) {
+        System.out.println(carta1);
+        } else {
+            System.out.println(carta2);
+        }
+    }
+}
