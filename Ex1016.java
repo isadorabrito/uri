287255bf3ae064f10a7107695228731bfa2e2ea0
Isadora Brito <isadoradpbrito@gmail.com>
2021-02-11 18:08:08 -0300

    uri

---
+++ Ex1016.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Ex1016 {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int n,t;
+        n = in.nextInt();
+
+        t = n * 2;
+        System.out.println(+t+ " minutos");
+    }
+}
