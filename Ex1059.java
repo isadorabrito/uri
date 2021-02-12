cf292ca669ee28ab8f51ab0f1f84009871ed614e
Isadora Brito <isadoradpbrito@gmail.com>
2021-02-11 21:31:38 -0300

    uri

---
+++ Ex1059.java
@@ -0,0 +1,11 @@
+public class Ex1059 {
+    public static void main(String[] args) {
+
+        int cont = 0;
+
+        while (cont < 100) {
+            cont +=2;
+            System.out.println(cont);
+        }
+    }
+}
